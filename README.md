139deb1d7fd8a9ac436ede6a06621dd2b2483287
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-04-26 19:18:29 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# github
+# FireGitHubBase
 Created with CodeSandbox
---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCW1ptIqcpIDXGrF9-KujfGtqzHTpyI0gY",
+  authDomain: "githubfirebase-tushar.firebaseapp.com",
+  projectId: "githubfirebase-tushar",
+  storageBucket: "githubfirebase-tushar.appspot.com",
+  messagingSenderId: "583077292527",
+  appId: "1:583077292527:web:1cf088fd3268fde4bc6e1a",
+  measurementId: "G-JCBHZR6PJJ"
+};
